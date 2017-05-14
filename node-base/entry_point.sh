98c059fc0c551ca0ee0f68c39ac789a29cda92bc
Eike-Christian Ramcke <dein.christian@googlemail.com>
2017-05-14 13:10:56 +0200

    Initial commit

---
+++ node-base/entry_point.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+source /opt/bin/functions.sh
+/opt/selenium/generate_config > /opt/selenium/config.json
+cat /opt/selenium/config.json
+
+export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
+
+if [ ! -e /opt/selenium/config.json ]; then
+  echo No Selenium Node configuration file, the node-base image is not intended to be run directly. 1>&2
+  exit 1
+fi
+
+if [ -z "$HUB_PORT_4444_TCP_ADDR" ]; then
+  echo Not linked with a running Hub container 1>&2
+  exit 1
+fi
+
+function shutdown {
+  kill -s SIGTERM $NODE_PID
+  wait $NODE_PID
+}
+
+if [ ! -z "$REMOTE_HOST" ]; then
+  >&2 echo "REMOTE_HOST variable is *DEPRECATED* in these docker containers.  Please use SE_OPTS=\"-host <host> -port <port>\" instead!"
+  exit 1
+fi
+
+if [ ! -z "$SE_OPTS" ]; then
+  echo "appending selenium options: ${SE_OPTS}"
+fi
+
+# TODO: Look into http://www.seleniumhq.org/docs/05_selenium_rc.jsp#browser-side-logs
+
+SERVERNUM=$(get_server_num)
+
+rm -f /tmp/.X*lock
+
+xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
+  java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
+    -role node \
+    -hub http://$HUB_PORT_4444_TCP_ADDR:$HUB_PORT_4444_TCP_PORT/grid/register \
+    -nodeConfig /opt/selenium/config.json \
+    ${SE_OPTS} &
+NODE_PID=$!
+
+trap shutdown SIGTERM SIGINT
+wait $NODE_PID
