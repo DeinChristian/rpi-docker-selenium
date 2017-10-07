4e140e04d3e2a65f2e2c3c1c3222e206de939f51
Eike-Christian Ramcke <dein.christian@googlemail.com>
2017-10-07 23:34:36 +0200

    Upgrade to Selenium 3.5.3

--- node-chrome-debug/build.sh
+++ node-chrome-debug/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 BUILD_LATEST_TAG="latest"
-BUILD_CURRENT_TAG="3.4.0"
+BUILD_CURRENT_TAG="3.5.3"
 
 docker build -t deinchristian/rpi-selenium-node-chrome-debug:${BUILD_LATEST_TAG} -t deinchristian/rpi-selenium-node-chrome-debug:${BUILD_CURRENT_TAG} . && \
 docker push deinchristian/rpi-selenium-node-chrome-debug
