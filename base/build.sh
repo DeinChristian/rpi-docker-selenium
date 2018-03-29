b56079a10537728d3043fcf9ed62cfe9cb8c3ce3
Eike-Christian Ramcke <dein.christian@googlemail.com>
2018-03-29 22:13:44 +0200

    Upgrade to Selenium 3.11.0

--- base/build.sh
+++ base/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 BUILD_LATEST_TAG="latest"
-BUILD_CURRENT_TAG="3.8.1"
+BUILD_CURRENT_TAG="3.11.0"
 
 docker build -t deinchristian/rpi-selenium-base:${BUILD_LATEST_TAG} -t deinchristian/rpi-selenium-base:${BUILD_CURRENT_TAG} . && \
 docker push deinchristian/rpi-selenium-base
